f932e1a5ca480669bdefc357a75fb67c6564cfa4
KyleDesigns <kyle.addison.designs@gmail.com>
2022-10-29 12:58:51 -0600

    Feedback Iterations
    All buttons should have a hover state, not just the ‘Download Resume’ button.
    Buttons could be bigger.
    Footer box should meet with the above background image (removed gap).
    Change buttons’ border color for hover state (blue).
    Change cursor to pointer finger on buttons during hover.
    Brighten Hero Profile Image.
    Edit & shorten ‘About Me’ section

--- js/index.js
+++ js/index.js
@@ -4,8 +4,10 @@ $( "button").hover(
     function() {
         $(this).css("background-color", "limegreen");
         $(this).css("color", "black");
+        $(this).css("border-color", "#333B50");
     }, function() {
         $(this).css("background-color", "#333B50");
         $(this).css("color", "white");
+        $(this).css("border-color", "limegreen");
     }
 );
